8da352e2e93824316255e37aff1a818bc7d0a690
OltVr <106235563+OltVr@users.noreply.github.com>
2024-01-07 21:57:46 +0100

    U bo multiplekseri 8 ne 1

--- KODI/mux8ne1.v
+++ KODI/mux8ne1.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 01/04/2024 04:28:03 PM
 // Design Name: 
-// Module Name: mux4ne1
+// Module Name: mux8ne1
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,14 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mux4ne1(
+module mux8ne1(
     input Hyrja0,
     input Hyrja1,
     input Hyrja2,
     input Hyrja3,
-    input [1:0] S,
+    input Hyrja4,
+    input [2:0] S,
     output Dalja
     );
     
-    assign Dalja= S[1] ? (S[0]? Hyrja3:Hyrja2) : (S[0]? Hyrja1:Hyrja0); 
+    assign Dalja= S[2] ? Hyrja4 : (S[1]? (S[0]? Hyrja3:Hyrja2):(S[0]? Hyrja1:Hyrja0)); 
 endmodule
