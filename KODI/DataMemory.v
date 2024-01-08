a642012340fbc444cee1f64067158ce9222fa7cb
VleraDerplanin <116980518+VleraDerplanin@users.noreply.github.com>
2024-01-08 20:55:07 +0100

    Update DataMemory.v

--- KODI/DataMemory.v
+++ KODI/DataMemory.v
@@ -1,13 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/07/2024 05:00:47 PM
+// Design Name: 
+// Module Name: DataMemory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module DataMemory(
   input [15:0] Adresa,
   input [15:0] WriteData,
   input Clock,
   input MemWrite,
   input MemRead,
-  output [15:0] ReadData
+  output reg [15:0] ReadData
 );
   reg [7:0] DataMem[127:0];
-  initial $readmemh("dataMemory.mem",DataMem);
+  initial $readmemb("dataMemory.mem",DataMem);
   
   always @(posedge Clock)
     begin
@@ -16,12 +37,13 @@ module DataMemory(
         DataMem[Adresa] <= WriteData[15:8];
       DataMem[Adresa+1] <= WriteData[7:0];
         end
+        ReadData={DataMem[Adresa],DataMem[Adresa+1]};
     end 
   always @(negedge Clock)
     begin
-      $writememh("dataMemory.mem",DataMem);
+    if(MemWrite)
+      $writememb("dataMemory.mem",DataMem);
     end
-  
-  assign ReadData={DataMem[Adresa],DataMem[Adresa+1]};
+ 
   
 endmodule
