566bd5bc8cc93aba931693eea01444b2afc01dbf
OltVr <106235563+OltVr@users.noreply.github.com>
2024-01-08 21:26:49 +0100

    Update ALUcontrol_unit.v

--- KODI/ALUcontrol_unit.v
+++ KODI/ALUcontrol_unit.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 05/13/2022 11:13:47 AM
+// Create Date: 01/07/2024 07:59:42 PM
 // Design Name: 
-// Module Name: ALUControl
+// Module Name: ALUcontrol_unit
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -18,9 +18,6 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-//ALU Control (CU_OUT_x, Function Code nga R-formati, Opcode, T19) - eshte shtuar ALUOp per I-format qe nuk eshte ne foto po kerkohet ne detyre
-
 module ALUcontrol_unit(
     input [1:0] ALUOp, 
     input [1:0] Funct, 
@@ -28,33 +25,38 @@ module ALUcontrol_unit(
     output reg [3:0] Operacioni
 );
 
-always @(ALUOp)
+always @(ALUOp or Funct or opcode)
 begin
-    case(ALUOp)
-        2'b00: Operacioni = 4'b0100;  //LW, SW 
-        2'b01: Operacioni = 4'b1100;  //BEQ
+    case (ALUOp)
+        2'b00: Operacioni <= 4'b0100; //LW,SW 
+        2'b01: Operacioni <= 4'b1100; //BEQ
         2'b10:
-            case(Funct)
-                2'b00: Operacioni = (opcode == 4'b0000) ? 4'b0000 : 4'b0100;  //AND or ADD
+            case (Funct)
+                2'b00: 
+                    case (opcode)
+                        4'b0000: Operacioni <= 4'b0000; //AND
+                        4'b0001: Operacioni <= 4'b0100; //ADD
+                    endcase
+                2'b01:              
+                    case (opcode)
+                        4'b0000: Operacioni <= 4'b0010; //OR
+                        4'b0001: Operacioni <= 4'b1100; //SUB
+                    endcase
+                2'b10: Operacioni <= 4'b0011; //XOR
             endcase
-        2'b01:  //R-format
-            case(opcode)
-                4'b0000: Operacioni = 4'b0010;  //OR
-                4'b0001: Operacioni = 4'b1100;  //SUB
-            endcase
-        2'b10: Operacioni = 4'b0011;  //XOR
-        2'b11:  //I-format
-            case(opcode)
-                4'b1001: Operacioni = 4'b0100;  //ADDI
-                4'b1010: Operacioni = 4'b1101;  //SUBI
-                4'b1011: Operacioni = 4'b0001;  //SLTI
+        2'b11: //I-format
+            case (opcode)
+                4'b1001: Operacioni <= 4'b0100; // per ADDI
+                4'b1010: Operacioni <= 4'b1101; // per SUBI
+                4'b1011: Operacioni <= 4'b0001; // per SLTI
                 4'b0010:
-                    case(Funct)
-                        2'b00: Operacioni = 4'b0110;  //SLL - bonus
-                        2'b01: Operacioni = 4'b0111;  //SRA - bonus
+                    case (Funct)
+                        2'b00: Operacioni <= 4'b0110; // per SLL - bonus
+                        2'b01: Operacioni <= 4'b0111; // per SRA - bonus
                     endcase
             endcase
     endcase
 end
 
 endmodule
+
