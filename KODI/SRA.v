fc50d5ffd5547ff651b9aa94bedfa6b766a81d81
Leutrim Morina <116391183+LeutrimMorina13@users.noreply.github.com>
2024-01-08 15:08:17 +0100

    SRA

---
+++ KODI/SRA.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/08/2024 12:54:20 AM
+// Design Name: 
+// Module Name: SRA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SRA(
+    input signed [15:0] hyrja,
+    input [3:0] shamt,
+    output signed [15:0] dalja
+    );
+    
+    assign dalja = hyrja >> shamt;
+    
+endmodule
