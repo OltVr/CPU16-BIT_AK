a79f2d9fa40208a40fe3defea6e9e1366273a7ac
OltVr <106235563+OltVr@users.noreply.github.com>
2024-01-19 22:52:48 +0100

    Update ACUT.v

--- Test/ACUT.v
+++ Test/ACUT.v
@@ -29,40 +29,33 @@ module ACUT(
   reg [3:0] opcode;
   wire [3:0] ALUCtrl;
 
-  // Instantiate ALUControl module
-  ALUControl dut (
+  ALUControl ALUCT (
     .ALUOp(ALUOp),
     .Funct(Funct),
     .opcode(opcode),
     .ALUCtrl(ALUCtrl)
   );
 
-  // Test scenario
   initial begin
-    // Test Case 1: LW (ALUOp=00, Funct=00, opcode=0000)
     ALUOp = 2'b00;
     Funct = 2'b00;
     opcode = 4'b0000;
-    #10; // Wait for some simulation time
+    #10; 
     $display("Test Case 1: ALUCtrl = %b", ALUCtrl);
 
-    // Test Case 2: SUB (ALUOp=10, Funct=01, opcode=0001)
+    
     ALUOp = 2'b10;
     Funct = 2'b01;
     opcode = 4'b0001;
-    #10; // Wait for some simulation time
+    #10; 
     $display("Test Case 2: ALUCtrl = %b", ALUCtrl);
 
-    // Test Case 3: SLL (ALUOp=11, Funct=00, opcode=0010)
+  
     ALUOp = 2'b11;
     Funct = 2'b00;
     opcode = 4'b0010;
-    #10; // Wait for some simulation time
+    #10; 
     $display("Test Case 3: ALUCtrl = %b", ALUCtrl);
-
-    // Add more test cases as needed
-
-    // End simulation
     $finish;
   end
 
