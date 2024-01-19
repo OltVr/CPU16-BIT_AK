6115b6fe492347b3bc0924eb58375df554dbc21d
OltVr <106235563+OltVr@users.noreply.github.com>
2024-01-19 22:57:33 +0100

    Update RFT.v

--- Test/RFT.v
+++ Test/RFT.v
@@ -1,13 +1,12 @@
 module RFT;
 
-  // Signals
   reg [1:0] RS, RT, RD;
   reg [15:0] WriteData;
   reg RegWrite, Clock;
   wire [15:0] ReadRS, ReadRT;
 
-  // Instantiate RegisterFile module
-  RegisterFile dut (
+
+  RegisterFile RFT (
     .RS(RS),
     .RT(RT),
     .RD(RD),
@@ -18,15 +17,15 @@ module RFT;
     .ReadRT(ReadRT)
   );
 
-  // Clock generation
+
   initial begin
     Clock = 0;
     forever #5 Clock = ~Clock;
   end
 
-  // Test scenario
+
   initial begin
-    // Initialize inputs
+
    #0 RS = 2'b00;
     RT = 2'b01;
     RD = 2'b01;
@@ -40,13 +39,13 @@ module RFT;
     WriteData = 16'h1234;
     RegWrite = 1;
 
-    // Apply inputs and observe outputs
-    #10; // Wait for some simulation time
+    
+    #10; 
     $display("ReadRS: %h, ReadRT: %h", ReadRS, ReadRT);
 
-    // Add more test scenarios as needed
 
-    // End simulation
+
+  
     $finish;
   end
 
